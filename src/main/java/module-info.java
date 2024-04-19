a49cc595453c75ae740a156a55c3932e77fbe25d
albrinBuzz <152460564+albrinBuzz@users.noreply.github.com>
2024-04-18 21:43:37 -0400

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.organizerfile {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.organizerfile to javafx.fxml;
-    exports org.example.organizerfile;
-}
